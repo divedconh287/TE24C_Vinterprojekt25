e81c87f5d85da48160b2700b4138270bc4fa5fe7
Aaron Granath TE24C <divedconh287@gmail.com>
2025-11-25 11:19:33 +0100

    under lektionen

--- TE24C Vinterprojekt 25/Program.cs
+++ TE24C Vinterprojekt 25/Program.cs
@@ -1,6 +1,8 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 
 Raylib.InitWindow(1000, 1000, "Vinter God Jul");
+Raylib.SetTargetFPS(60);
 
 Texture2D placeholder = Raylib.LoadTexture("placeholder.png");
 // hämtar bilden 
@@ -9,36 +11,48 @@ Rectangle placeholderrect = new(260, 500, placeholder.Dimensions);
 float speed = 4;
 // hastighet
 
-Raylib.BeginDrawing();
+List<Vector2> stars = [];
 
-    Raylib.ClearBackground(Color.Black);
-    Random rand = new Random();
-for (int star = 0; star < 50; star++)
-// räknar stjärnor upp till 50 
-{
-    Raylib.DrawCircle(rand.Next(15, 985), rand.Next(15, 985), 5, Color.White);
-    // rand.Next() gör så att stjärnorna placeras mellan pixel 15 och pixel 985
-    // 15 och 985 gör så att det är 15px mellanrum mellan yttersta stjärnan & bordern
-}// Sixten Tamleht hjälpte med Random grejerna & förklarade hur for loopen funkar men jag förstår
+Random rand = new Random();
+    for (int star = 0; star < 50; star++)
+    // räknar stjärnor upp till 50 
+    {
+        Vector2 starPos = new(rand.Next(15,985), rand.Next(15,985));
+        stars.Add(starPos);
+        // krankhjälp^
 
-Raylib.DrawTexture(placeholder,(int)placeholderrect.X,(int)placeholderrect.Y,Color.White);
+        // Raylib.DrawCircle(rand.Next(15, 985), rand.Next(15, 985), 5, Color.White);
+        // den övre kommentaren funkariiish 
+        // rand.Next() gör så att stjärnorna placeras mellan pixel 15 och pixel 985
+        // 15 och 985 gör så att det är 15px mellanrum mellan yttersta stjärnan & bordern
+    }// Sixten Tamleht hjälpte med Random grejerna & förklarade hur for loopen funkar men jag förstår
 
-Raylib.EndDrawing();
 
 while (Raylib.WindowShouldClose() == false)
 {
-    Raylib.BeginDrawing();
-    if (Raylib.IsKeyDown(KeyboardKey.Right)) placeholderrect.X += speed;
-    if (Raylib.IsKeyDown(KeyboardKey.Left)) placeholderrect.X -= speed;
-    if (Raylib.IsKeyDown(KeyboardKey.Down)) placeholderrect.Y += speed;
-    if (Raylib.IsKeyDown(KeyboardKey.Up)) placeholderrect.Y -= speed;
+    if (Raylib.IsKeyDown(KeyboardKey.W)) placeholderrect.Y -= speed;
+    if (Raylib.IsKeyDown(KeyboardKey.A)) placeholderrect.X -= speed;
+    if (Raylib.IsKeyDown(KeyboardKey.S)) placeholderrect.Y += speed;
+    if (Raylib.IsKeyDown(KeyboardKey.D)) placeholderrect.X += speed;
 
-    if (placeholderrect.X < 0) placeholderrect.X += speed;
-    if (placeholderrect.X + placeholderrect.Width > 1000) placeholderrect.X -= speed;
     if (placeholderrect.Y < 0) placeholderrect.Y += speed;
+    if (placeholderrect.X + placeholderrect.Width > 1000) placeholderrect.X -= speed;
     if (placeholderrect.Y + placeholderrect.Height > 1000) placeholderrect.Y -= speed;
+    if (placeholderrect.X < 0) placeholderrect.X += speed;
+
+
+    Raylib.BeginDrawing();
     // kontrollgrejer, bildinläggning & hastighet kopierade & modifierade från github.com/divedconh287/TE24C_Graphics
+    Raylib.ClearBackground(Color.Black);
+    for (int i = 0; i < stars.Count; i++)
+    {
+        Raylib.DrawCircleV(stars[i], 5, Color.White);
+    }//krankhjälp^
+
+    Raylib.DrawTexture(placeholder, (int)placeholderrect.X, (int)placeholderrect.Y, Color.White);
+
+
     Raylib.EndDrawing();
 }
 
-// vet inte vad som händer måste fråga Krank
+// krank rekommenderade att ha stjärnorna gå långsamt ned som en "starfield effekt"
