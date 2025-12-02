2de4c10ac1efc083752a04d6bf7c8ca30280f695
Aaron Granath TE24C <divedconh287@gmail.com>
2025-12-02 11:14:57 +0100

    under lektion!!! jag är dålig på att fråga

--- TE24C Vinterprojekt 25/Program.cs
+++ TE24C Vinterprojekt 25/Program.cs
@@ -13,13 +13,14 @@ Rectangle placeholderrect = new(500, 500, placeholder.Dimensions);
 float speed = 8;
 // hastighet
 
-List<Vector2> stars = [];
+List<Vector3> stars = [];
 
 Random rand = new Random();
 for (int star = 0; star < 5000; star++)
 // räknar stjärnor upp till 5000
 {
-    Vector2 starPos = new(rand.Next(15, 1905), rand.Next(15, 1065));
+    Vector3 starPos = new(rand.Next(15, 1905), rand.Next(15, 1065), 1+(float)rand.NextDouble() * 2);
+    // 1+ så att ingen står still, NextDouble är likt en float men väljer mellan 0 & 1 och därför behöver man gångra
     stars.Add(starPos);
     // krankhjälp^
 
@@ -36,34 +37,47 @@ while (Raylib.WindowShouldClose() == false)
     if (Raylib.IsKeyDown(KeyboardKey.A)) placeholderrect.X -= speed;
     if (Raylib.IsKeyDown(KeyboardKey.S)) placeholderrect.Y += speed;
     if (Raylib.IsKeyDown(KeyboardKey.D)) placeholderrect.X += speed;
+    if (Raylib.IsKeyDown(KeyboardKey.Space))
+    {
+    
+    }
 
     if (placeholderrect.Y < 0)
     {
         float Wdiff = -placeholderrect.Y;
         placeholderrect.Y += Wdiff;
     }
-    if (placeholderrect.X + placeholderrect.Width > 1920)
+    if (placeholderrect.X < 0)
     {
-        float diff = placeholderrect.X + placeholderrect.Width - 1920;
-        placeholderrect.X -= diff;
-    }// Krank måsvingegrej
+        float Adiff = -placeholderrect.X;
+        placeholderrect.X += Adiff;
+    }
     if (placeholderrect.Y + placeholderrect.Height > 1080)
     {
         float Sdiff = placeholderrect.Y + placeholderrect.Height - 1080;
         placeholderrect.Y -= Sdiff;
     }
-    if (placeholderrect.X < 0) placeholderrect.X += speed;
+    if (placeholderrect.X + placeholderrect.Width > 1920)
     {
-        
-    }
+        float Ddiff = placeholderrect.X + placeholderrect.Width - 1920;
+        placeholderrect.X -= Ddiff;
+    }// Krank måsvingegrej
 
 
     Raylib.BeginDrawing();
-    // kontrollgrejer, bildinläggning & hastighet kopierade & modifierade från github.com/divedconh287/TE24C_Graphics
+    // kontrollgrejer, bildinläggning & hastighet kopierade & modifierade från https://github.com/divedconh287/TE24C_Graphics
     Raylib.ClearBackground(Color.Black);
     for (int i = 0; i < stars.Count; i++)
     {
-        Raylib.DrawCircleV(stars[i], 2, Color.White);
+        Vector3 s = stars[i];
+        s.Y += s.Z;
+        if(s.Y > 1080) s.Y = 0;
+        // likt rect^
+        stars[i] = s;
+        // gör så att sjärnorna åker nedåt
+
+        // hakparentes är till listor
+        Raylib.DrawCircle((int)stars[i].X, (int)stars[i].Y, 2, Color.White);
     }//krankhjälp^
 
     Raylib.DrawTexture(placeholder, (int)placeholderrect.X, (int)placeholderrect.Y, Color.White);
